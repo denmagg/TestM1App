6c0e44d78aaa451b53bbe8f4ad6f6d40f123ad4a
Denis <denmagg.work@gmail.com>
2022-03-04 13:04:53 +0300

    Initial Commit

---
+++ TestM1App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestM1App
+//
+//  Created by Денис Медведев on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
